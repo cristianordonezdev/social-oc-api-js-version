072ad2c214e4875c868a33c97856dfec89ec13ab
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-07-05 09:21:39 -0500

    fix merged

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 'use strict'
 
 // IMPORTING
-const app = express();
 const express = require('express');
+const app = express();
 const body_parser = require('body-parser');
 const mysql = require('mysql');
 const express_myconnection = require('express-myconnection');
+require('dotenv').config();
+
 const db = {
     host: process.env.HOST_DB,
     user: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
     database: process.env.DATABASE_DB,
 };
-require('dotenv').config();
 
 // TO CONVERT DATA INTO JSON
 app.use(express.json());
