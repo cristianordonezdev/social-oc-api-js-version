31241ed969edecb1a219bc074d65678573230d0f
Crisitan Ramon Hernandez Ordoñez <chino@MacBook-Air-de-Crisitan.local>
2022-09-10 19:19:47 -0500

    service upload profile picture, service delete profile picture

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const uuid = require('uuid');
 const validator = require('validator');
+const cloudinary = require('../middlewares/cloudinary');
+const fs = require('fs');
 
 const controller = {
     follow: (request, response) => {
@@ -7,7 +9,7 @@ const controller = {
             if (err) return response.status(400).send({
                 message: err
             })
-            
+
             const params = request.body;
 
             con.query('SELECT * FROM followers WHERE user_follower_uuid = ? and user_followed_uuid = ?', [params.user_follower_uuid, params.user_followed_uuid], (err, rows) => {
@@ -21,7 +23,7 @@ const controller = {
                         if (err) return response.status(400).send({
                             message: err
                         })
-                
+
                         return response.status(200).send({
                             message: 'Unfollow',
                             rows
@@ -34,12 +36,12 @@ const controller = {
                         user_follower_uuid: params.user_follower_uuid,
                         user_followed_uuid: params.user_followed_uuid,
                     }
-        
+
                     con.query('INSERT INTO followers SET ? ', [follow], (err, rows) => {
                         if (err) return response.status(400).send({
                             message: err
                         })
-                
+
                         return response.status(200).send({
                             message: 'Following',
                             rows
@@ -94,7 +96,7 @@ const controller = {
         });
     },
 
-    followersList: (request, response)=> {
+    followersList: (request, response) => {
         request.getConnection((err, con) => {
             if (err) return response.status(400).send({
                 message: err
@@ -102,7 +104,7 @@ const controller = {
 
 
             const params = request.params;
-            
+
             con.query('SELECT U.uuid, U.name, U.nickname, U.profile_image FROM users U JOIN followers F ON U.uuid = F.user_follower_uuid AND F.user_followed_uuid = ?', [params.uuid], (err, rows) => {
                 if (err) return response.status(400).send({
                     message: err
@@ -112,13 +114,13 @@ const controller = {
                     if (err) return response.status(400).send({
                         message: err
                     });
-                    const followers_list = rows.filter((item) => item.uuid !== params.uuid)              
-                    const following_list = rows2.filter((item) => item.uuid !== params.uuid) 
-  
+                    const followers_list = rows.filter((item) => item.uuid !== params.uuid)
+                    const following_list = rows2.filter((item) => item.uuid !== params.uuid)
+
                     followers_list.map((item) => {
                         if (following_list.find((i) => i.uuid === item.uuid) !== undefined)
                             item['you_follow'] = true
-                        else 
+                        else
                             item['you_follow'] = false
                     })
                     return response.status(200).send({
@@ -130,7 +132,7 @@ const controller = {
         });
     },
 
-    followingList: (request, response)=> {
+    followingList: (request, response) => {
         request.getConnection((err, con) => {
             if (err) return response.status(400).send({
                 message: err
@@ -138,13 +140,13 @@ const controller = {
 
 
             const params = request.params;
-            
+
             con.query('SELECT U.uuid, U.name, U.nickname, U.profile_image FROM users U JOIN followers F ON U.uuid = F.user_followed_uuid AND F.user_follower_uuid = ?', [params.uuid], (err, rows) => {
                 if (err) return response.status(400).send({
                     message: err
                 });
 
-                const following_list = rows.filter((item) => item.uuid !== params.uuid)              
+                const following_list = rows.filter((item) => item.uuid !== params.uuid)
 
                 following_list.map((item) => {
                     item['you_follow'] = true
@@ -189,11 +191,11 @@ const controller = {
                     gender: params.gender,
                     email: params.email,
                 };
-            
+
                 con.query('UPDATE users SET ? WHERE uuid = ? ', [user_updated, params.uuid], (err, rows) => {
                     if (err) return response.status(400).send({
                         err
-                    });  
+                    });
 
                     return response.status(400).send({
                         status: 'ok',
@@ -214,12 +216,12 @@ const controller = {
                 message: err
             });
 
-            
+
             //GET AN ALGORHIT TO MAKE SUGGESTIONS
 
             //THE LOGIC WILL BE ABLE TO SHOW MY NO FOLLOWERS, HOWEVER IS NOT THE BEST PRACTICE, TRY TO RESOLVE THIS
             const uuid = request.params.uuid;
-            
+
             con.query('SELECT U.uuid, U.name, U.nickname, U.profile_image FROM users U JOIN followers F ON U.uuid = F.user_followed_uuid AND F.user_follower_uuid = ?', [uuid], (err, rows) => {
                 if (err) return response.status(400).send({
                     message: err
@@ -251,6 +253,62 @@ const controller = {
             })
         });
     },
+
+    uploadProfilePicture: (request, response) => {
+      request.getConnection((err, con) => {
+        if (err) return response.status(400).send({
+          message: err
+        });
+        const user_uuid = request.body.user_uuid;
+        const picture = request.files[0];
+        const uploadImage = async () => {
+          const uploader = async (path) => await cloudinary.uploads(path, 'socialOC');
+            let url = {};
+            const {path} = picture;
+            const new_path = await uploader(path);
+            url = new_path;
+            fs.unlinkSync(path);
+            return url;
+        };
+
+        uploadImage().then((response_upload) => {
+          const obj_updated = {
+            profile_image: response_upload.url,
+          };
+          con.query('UPDATE users SET ? WHERE uuid = ?', [obj_updated, user_uuid], (err, res_updated) => {
+            return response.status(200).send({
+                message: 'The profile picture has been uploaded',
+                profile_image: response_upload.url
+            });
+          });
+        });
+      });
+    },
+
+    deleteProfilePicture: (request, response) => {
+        request.getConnection((err, con) => {
+          if (err) return response.status(400).send({
+            message: err
+          });
+          const user_uuid = request.body.user_uuid;
+
+          con.query('SELECT profile_image FROM users WHERE uuid = ?', [user_uuid], (err, rows) => {
+            const image = rows[0].profile_image;
+            const deleter = async (path) => await cloudinary.delete(path);
+            deleter(`socialOC/${image.split('/')[8].split('.')[0]}`);
+          });
+
+          const obj_updated = {
+            profile_image: null
+          };
+          con.query('UPDATE users SET ? WHERE uuid = ?', [obj_updated, user_uuid], (err, rows) => {
+            return response.status(200).send({
+              message: 'The profile picture has been deleted',
+              profile_image: null
+            });
+          });
+        });
+      }
 }
 
 module.exports = controller;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
-// const upload = require('../middlewares/multer');
+const upload = require('../middlewares/multer');
 
 // router.post('/upload-post', upload.array('images'), controller.uploadPost);
 router.post('/follow', controller.follow);
@@ -11,6 +11,8 @@ router.post('/get-user', controller.getUser);
 router.get('/followers-list/:uuid', controller.followersList);
 router.get('/following-list/:uuid', controller.followingList);
 router.get('/suggestions/:uuid', controller.getSuggestions);
+router.post('/upload-profile-picture', upload.array('picture'), controller.uploadProfilePicture);
+router.put('/delete-profile-picture', controller.deleteProfilePicture);
 
 router.put('/edit-user', controller.editUser);
 
